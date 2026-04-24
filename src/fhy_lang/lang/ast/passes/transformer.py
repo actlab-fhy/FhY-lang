74b8590d87831450074c42128d083590633e11cf
Christopher Priebe <chrisp9090@gmail.com>
2026-04-24 11:59:58 -0700

    fix(lang.ast.passes.transformer): Fixed transformer pass.

--- src/fhy_lang/lang/ast/passes/transformer.py
+++ src/fhy_lang/lang/ast/passes/transformer.py
@@ -291,6 +291,7 @@ class Transformer(VisitablePass[Node, Node]):
             self._namespace_stack.pop()
 
         return ForAllStatement(
+            name=node.name,
             index=new_index,
             body=new_body,
             provenance=node.provenance,
